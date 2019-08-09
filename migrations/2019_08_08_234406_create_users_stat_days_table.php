8c0567cbbbf57b4aa434d11804382c0904a2a8ce
Lucas <liangsan1998@gmail.com>
2019-08-09 20:05:55 +0800

    1

--- migrations/2019_08_08_234406_create_users_stat_days_table.php
+++ migrations/2019_08_08_234406_create_users_stat_days_table.php
@@ -12,31 +12,119 @@ class CreateUsersStatDaysTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users_stat_days', function(Blueprint $table)
+		Schema::create('users_stat_day', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->integer('top_id');
 			$table->integer('parent_id');
 			$table->integer('user_id');
-			$table->string('rid', 256)->default('');
+
 			$table->string('username', 32);
 			$table->string('lottery_id', 32);
 			$table->string('method_id', 32);
-			$table->integer('bets')->unsigned()->default(0);
-			$table->integer('points')->unsigned()->default(0);
-			$table->integer('bonus')->unsigned()->default(0);
-			$table->integer('canceled')->unsigned()->default(0);
-			$table->integer('team_bets')->unsigned()->default(0);
-			$table->integer('team_points')->unsigned()->default(0);
-			$table->integer('team_bonus')->unsigned()->default(0);
-			$table->integer('team_canceled')->unsigned()->default(0);
+
+            $table->unsignedBigInteger('recharge_amount')->default(0);
+            $table->integer('recharge_count')->default(0);
+            $table->integer('recharge_first')->default(0);
+
+            $table->unsignedBigInteger('withdraw_amount')->default(0);
+            $table->integer('withdraw_count')->default(0);
+
+            $table->unsignedBigInteger('system_transfer_in')->default(0);
+            $table->unsignedBigInteger('system_transfer_out')->default(0);
+            $table->unsignedBigInteger('parent_transfer_in')->default(0);
+
+            $table->unsignedBigInteger('salary')->default(0);
+            $table->unsignedBigInteger('dividend')->default(0);
+            $table->unsignedBigInteger('gift')->default(0);
+
+			$table->unsignedBigInteger('bets')->default(0);
+			$table->unsignedBigInteger('self_points')->default(0);
+            $table->unsignedBigInteger('child_points')->default(0);
+			$table->unsignedBigInteger('bonus')->default(0);
+			$table->unsignedBigInteger('canceled')->default(0);
+
+			$table->unsignedBigInteger('team_recharge_amount')->default(0);
+			$table->unsignedBigInteger('team_recharge_count')->default(0);
+			$table->unsignedBigInteger('team_withdraw_amount')->default(0);
+			$table->unsignedBigInteger('team_withdraw_count')->default(0);
+            $table->unsignedBigInteger('team_system_transfer_in')->default(0);
+            $table->unsignedBigInteger('team_system_transfer_out')->default(0);
+            $table->unsignedBigInteger('team_parent_transfer_in')->default(0);
+
+            $table->unsignedBigInteger('team_salary')->default(0);
+            $table->unsignedBigInteger('team_dividend')->default(0);
+            $table->unsignedBigInteger('team_gift')->default(0);
+
+            $table->unsignedBigInteger('team_bets')->default(0);
+            $table->unsignedBigInteger('team_self_points')->default(0);
+            $table->unsignedBigInteger('team_child_points')->default(0);
+            $table->unsignedBigInteger('team_bonus')->default(0);
+            $table->unsignedBigInteger('team_canceled')->default(0);
+
 			$table->integer('day');
 			$table->timestamps();
-			$table->index(['top_id','rid','day'], 'user_sale_day_top_id_rid_day_index');
-			$table->index(['top_id','parent_id','day'], 'user_sale_day_top_id_parent_id_day_index');
-			$table->index(['top_id','day'], 'user_sale_day_top_id_day_index');
-			$table->index(['top_id','user_id','day'], 'user_sale_day_top_id_user_id_day_index');
+
+			$table->index(['top_id','parent_id','day'], 'top_parent_day_index');
+			$table->index(['top_id','day'], 'top_day_index');
+			$table->index(['top_id','user_id','day'], 'top_user_id_day_index');
 		});
+
+        Schema::create('users_stat', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('top_id');
+            $table->integer('parent_id');
+            $table->integer('user_id');
+
+            $table->string('username', 32);
+            $table->string('lottery_id', 32);
+            $table->string('method_id', 32);
+
+            $table->unsignedBigInteger('recharge_amount')->default(0);
+            $table->integer('recharge_count')->default(0);
+
+            $table->unsignedBigInteger('withdraw_amount')->default(0);
+            $table->integer('withdraw_count')->default(0);
+
+            $table->unsignedBigInteger('system_transfer_in')->default(0);
+            $table->unsignedBigInteger('system_transfer_out')->default(0);
+            $table->unsignedBigInteger('parent_transfer_in')->default(0);
+
+            $table->unsignedBigInteger('salary')->default(0);
+            $table->unsignedBigInteger('dividend')->default(0);
+            $table->unsignedBigInteger('gift')->default(0);
+
+            $table->unsignedBigInteger('bets')->default(0);
+            $table->unsignedBigInteger('self_points')->default(0);
+            $table->unsignedBigInteger('child_points')->default(0);
+            $table->unsignedBigInteger('bonus')->default(0);
+            $table->unsignedBigInteger('canceled')->default(0);
+
+            $table->unsignedBigInteger('team_recharge_amount')->default(0);
+            $table->unsignedBigInteger('team_recharge_count')->default(0);
+            $table->unsignedBigInteger('team_withdraw_amount')->default(0);
+            $table->unsignedBigInteger('team_withdraw_count')->default(0);
+            $table->unsignedBigInteger('team_system_transfer_in')->default(0);
+            $table->unsignedBigInteger('team_system_transfer_out')->default(0);
+            $table->unsignedBigInteger('team_parent_transfer_in')->default(0);
+
+            $table->unsignedBigInteger('team_salary')->default(0);
+            $table->unsignedBigInteger('team_dividend')->default(0);
+            $table->unsignedBigInteger('team_gift')->default(0);
+
+            $table->unsignedBigInteger('team_bets')->default(0);
+            $table->unsignedBigInteger('team_self_points')->default(0);
+            $table->unsignedBigInteger('team_child_points')->default(0);
+            $table->unsignedBigInteger('team_bonus')->default(0);
+            $table->unsignedBigInteger('team_canceled')->default(0);
+
+            $table->timestamps();
+
+            $table->index(['top_id','parent_id'], 'top_parent_index');
+            $table->index(['top_id'], 'top_index');
+            $table->index(['top_id','user_id'], 'top_user_id_index');
+        });
 	}
 
 
@@ -47,7 +135,8 @@ class CreateUsersStatDaysTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users_stat_days');
+		Schema::drop('users_stat_day');
+        Schema::drop('users_stat');
 	}
 
 }
